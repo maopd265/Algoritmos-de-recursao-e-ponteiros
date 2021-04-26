af1ceff98d524ad0ba4285340f8c8d5896e48d32
BAVARESCO-PC\Bavaresco <maopdtec@gmail.com>
2021-04-26 15:41:38 -0300

    Exercicio recursivo 9

---
+++ recursao9.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+//Escrever uma função recursiva que calcule o valor de a elevado a b
+
+
+float potencia(float base, float expoente);
+
+int main ()
+{
+	
+	
+ system("color a");
+ 
+  setlocale(LC_ALL, "Portuguese");
+  printf("Utilizando caracteres e acentuação da língua portuguesa!\n\n");
+
+  system("pause");
+   
+	
+	return 0;
+}
+
+float potencia(float base, float expoente)
+{
+	
+}
