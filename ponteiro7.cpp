950a0ccee8526c893182aa25a8f8b2bd257289af
BAVARESCO-PC\Bavaresco <maopdtec@gmail.com>
2021-04-30 17:12:56 -0300

    ordenacao

--- ponteiro7.cpp
+++ ponteiro7.cpp
@@ -1,21 +1,14 @@
 #include<stdio.h>
-//BUBBLE SORT
+#include<stdlib.h>
+//QUICK SORT
+int compara(const void *x, const void *y)
+{
+	return (int)(*(int*)x-*(int*)y);
+}
 void ordenacaoVetor(int *vetor)
 {
-	static int varTemporaria;
-	for(int i=0;i<20;i++)
-	{
-		for(int j=i+1;j<20;j++)
-		{
-			if(vetor[i]>vetor[j])
-			{
-				varTemporaria=vetor[i];
-				vetor[i]=vetor[j];
-				vetor[j]=varTemporaria;
-			}
-		}
-		
-	}
+	 
+	qsort(vetor,20,sizeof(int),compara);
 }
 void escreveVetor(int vetor[])
 {
@@ -26,10 +19,11 @@ void escreveVetor(int vetor[])
 }
 int main()
 {
-int vetor[20]={9,1,7,6,3,4,1,7,3,2,9,1,7,6,3,4,1,7,3,2};
+	int vetor[20]={9,1,7,6,3,4,1,7,3,2,9,1,7,6,3,4,1,7,3,2};
 	escreveVetor(vetor);
 	printf("\n------------------------------------------------------------\n");
-	ordenacaoVetor(vetor);
+	//ordenacaoVetor(vetor);
+		qsort(vetor,20,sizeof(int),compara);
 	escreveVetor(vetor);
 	return 0;
 }
