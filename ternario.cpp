acd1a07994af400136b5ae56d5573abe9aa70c1b
BAVARESCO-PC\Bavaresco <maopdtec@gmail.com>
2021-05-06 21:12:00 -0300

    Exemplo de escrita ternária

---
+++ ternario.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main()
+{
+	int flag;
+	flag=1;
+	printf("\n%d",(flag<10?flag:0));
+	flag=10;
+	printf("\n%d",(flag<10?flag:0));
+	
+}
